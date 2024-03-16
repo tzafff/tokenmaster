95d64cb581d0a393e6ff7b5ca485e7bfc4a5dcf8
tzafff <christzaf99@gmail.com>
2024-03-16 17:19:25 +0200

    Navigation Component

--- src/App.js
+++ src/App.js
@@ -14,13 +14,33 @@ import TokenMaster from './abis/TokenMaster.json'
 import config from './config.json'
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async () => {
+    
+
+    // Refresh Account
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account)
+    })
+  }
+
+  useEffect(() => {
+    
+    loadBlockchainData()
+    
+  },[])
 
   return (
     <div>
       <header>
-
+        <Navigation account={account} setAccount={setAccount} />
         <h2 className="header__title"><strong>Welcome to Tokenmaster</strong></h2>
       </header>
+
+      <p>{account}</p>
 
     </div>
   );
